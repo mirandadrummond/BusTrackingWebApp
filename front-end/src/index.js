c3239be80969d55263692b372019d3fa6d7f7f85
Matt-Blanco <mattmblanco@gmail.com>
2022-11-27 14:56:41 +0100

    installed react router

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+          <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<App />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
+  </BrowserRouter>
 );
