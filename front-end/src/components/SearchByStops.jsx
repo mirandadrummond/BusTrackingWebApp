cd3116799991ce9b124b11f7d50da996fb51a1a7
Matt-Blanco <mattmblanco@gmail.com>
2022-11-28 11:24:04 +0100

    update elements on error

--- front-end/src/components/SearchByStops.jsx
+++ front-end/src/components/SearchByStops.jsx
@@ -1,16 +1,16 @@
 import { MenuItem, InputLabel, Select, Button } from "@mui/material";
 import { Container } from "@mui/system";
 import * as React from 'react'
-import { useEffect, useState } from 'react'; 
-import { getBusStops } from "../services/fetch";
+import { useEffect, useState } from 'react';
 import axios from 'axios'
+import { Link } from "react-router-dom";
 
 export function SearchByStops() {
-    const [startStop, updateStartStop] = React.useState('');
-    const [endStop, updateEndStop] = React.useState('');
-
-    // Call List of Stops from Database!
-    const listOfStops = ['A', 'B', 'C', 'D']
+    const [startStop, updateStartStop] = useState('');
+    const [endStop, updateEndStop] = useState('');
+    const [stops, setStops] = useState(['Not Loaded']);
+    const [loading, isLoading] = useState(true);
+    const [error, isError] = useState(false);
 
     const handleStartChange = (event, newStartStop) => {
         updateStartStop(newStartStop.props.value)
@@ -20,50 +20,56 @@ export function SearchByStops() {
         updateEndStop(newEndStop.props.value)
     }
 
-    // getBusStops().then(r => {
-    //     console.log(r)
-    // })
-
     useEffect(() => {
-        console.log("test1");
-        axios('http://localhost:4000/bus_stop').then(response => {
-            console.log(response.data)
-        }).catch(err => {
-            console.error(`Error fetching data: `, err)
-        })
+        if (stops[0] === 'Not Loaded') {
+            axios('http://localhost:4000/bus_stop').then(response => {
+                setStops(response.data.map(v => v.name))
+            }).catch(err => {
+                console.error(`Error fetching data: `, err)
+                isError(true);
+            }).finally(r => isLoading(false));
+        }
     })
 
     return (
-        <Container>
-            <InputLabel id="selectStartStop">Starting Stop</InputLabel>
-            <Select
-                id="selectStartStop"
-                value={startStop}
-                label="Starting Stop"
-                onChange={handleStartChange}
-            >
-                {
-                    listOfStops.filter(stop => stop !== endStop).map(filteredStop => {
-                        return (
-                            <MenuItem value={filteredStop}>{filteredStop}</MenuItem>
-                        )
-                    })
-                }
-            </Select>
-            <InputLabel id="selectEndStop">Ending Stop</InputLabel>
-            <Select
-                id="selectEndStop"
-                value={endStop}
-                label="Ending Stop"
-                onChange={handleEndChange}
-            >
-                {
-                    listOfStops.filter(stop => stop !== startStop).map(filteredStop => {
-                        return <MenuItem value={filteredStop}>{filteredStop}</MenuItem>
-                    })
-                }
-            </Select>
-            <Button>Search</Button>
-        </Container>
+        <div>
+            {
+                loading ? <div>Loading</div> :
+                    error ? <div>Encountered Error when loading. Please refresh the page.</div> :
+                        <Container>
+                            <InputLabel id="selectStartStop">Starting Stop</InputLabel>
+                            <Select
+                                id="selectStartStop"
+                                value={startStop}
+                                label="Starting Stop"
+                                onChange={handleStartChange}
+                            >
+                                {
+                                    stops.filter(stop => stop !== endStop).map(filteredStop => {
+                                        return (
+                                            <MenuItem value={filteredStop}>{filteredStop}</MenuItem>
+                                        )
+                                    })
+                                }
+                            </Select>
+                            <InputLabel id="selectEndStop">Ending Stop</InputLabel>
+                            <Select
+                                id="selectEndStop"
+                                value={endStop}
+                                label="Ending Stop"
+                                onChange={handleEndChange}
+                            >
+                                {
+                                    stops.filter(stop => stop !== startStop).map(filteredStop => {
+                                        return <MenuItem value={filteredStop}>{filteredStop}</MenuItem>
+                                    })
+                                }
+                            </Select>
+                            <Link to="stop_status">
+                                <Button>Search</Button>
+                            </Link>
+                        </Container>
+            }
+        </div>
     )
 }
