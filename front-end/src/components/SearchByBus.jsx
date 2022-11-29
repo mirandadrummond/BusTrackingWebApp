a9a1f51e963642ddaa8e3e9d2ba3a09f0c2f5aa4
Matt-Blanco <mattmblanco@gmail.com>
2022-11-29 19:24:47 +0100

    show active buses

--- front-end/src/components/SearchByBus.jsx
+++ front-end/src/components/SearchByBus.jsx
@@ -3,12 +3,15 @@ import { Container } from '@mui/system';
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom'
 import { getLink } from '../utils';
-import { useState } from 'react';
-
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 
 export function SearchByBus(props) {
     const location = useLocation();
     const [busNumber, setNumber] = useState(props.searchVal);
+    const [buses, setBuses] = useState([]);
+    const [loading, isLoading] = useState(true);
+    const [error, isError] = useState(false);
 
     const handleChange = (event) => {
         const re = /^[0-9\b]+$/;
@@ -18,12 +21,36 @@ export function SearchByBus(props) {
         }
     }
 
+    useEffect(() => {
+        axios('http://localhost:4000/buses').then(response => {
+            setBuses(response.data)
+        }).catch(err => {
+            console.error(`Error fetching data: `, err)
+            isError(true);
+        }).finally(res => isLoading(false))
+    }, [])
+
     return (
-        <Container id="searchByBus" className="actionBlock">
-            <TextField onChange={handleChange} value={busNumber} id="outlined-basic" label="Enter Bus Number" variant="outlined" />
-            <Link to={getLink(location, 'bus_status')} state={{busNum: parseInt(busNumber)}}>
-                <Button>Search</Button>
-            </Link>
-        </Container>
+        <>
+            {
+                loading ? <p>Loading please wait.</p> : error ? <p>Error loading data please refresh the page.</p> :
+                    <Container id="searchByBus" className="actionBlock">
+                        <TextField onChange={handleChange} value={busNumber} id="outlined-basic" label="Enter Bus Number" variant="outlined" />
+                        <Link to={getLink(location, 'bus_status')} state={{ busNum: parseInt(busNumber) }}>
+                            <Button>Search</Button>
+                        </Link>
+                        <p>Active Buses:</p>
+
+                        {
+                            buses.map(bus => {
+                                return (
+                                    <span>Bus: {bus.bus_number} </span>
+                                )
+                            })
+                        }
+
+                    </Container>
+            }
+        </>
     )
 }
